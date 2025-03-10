f5eb856703ae023ed9b06545cb08687845fa587a
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-03-09 20:48:53 -0400

    Task 4 - Support Ticket Resolution with Event Bubbling

--- cc_14.js
+++ cc_14.js
@@ -25,6 +25,12 @@ function createElement(customerName, issue, priorityLevel) {
     resolveButton.textContent = "Resolve Ticket";
     supportTicket.appendChild(resolveButton);
 
+    //TASK 4
+    // Attach event listener to remove employee button
+    resolveButton.addEventListener("click", function() {
+        resolveTicket(supportTicket);
+    });
+
     ticketContainerId.appendChild(supportTicket);
 }
 
@@ -48,3 +54,23 @@ supportTicketArray.forEach(ticket => {
         ticket.style.border = "3px solid lightcoral";
     }
 });
+
+// TASK 4: IMPLEMENTING TICKET RESOLUTION WITH EVENT BUBBLING
+
+// Add functionality to resolve ticket button
+function resolveTicket(ticket) {
+    ticketContainerId.removeChild(ticket);
+}
+
+// Attach event listener to Ticket Container
+ticketContainerId.addEventListener("click", function() {
+    console.log("Support ticket section clicked!");
+})
+
+// Attach event listener to each support ticket
+document.querySelectorAll(".support-ticket").forEach(ticket => {
+    ticket.addEventListener("click", function(event) {
+        console.log("Support ticket clicked!");
+        event.stopPropagation(); //Enable to prevent event bubbling
+    })
+})
